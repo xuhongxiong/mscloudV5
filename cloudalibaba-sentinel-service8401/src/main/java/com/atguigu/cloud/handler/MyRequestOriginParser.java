cff04e1e3611bb42e0d2795cc80b1e596667ce37
xuhongxiong <xuhongxiong@karrytech.com>
2024-03-03 00:37:24 +0800

    2024springcloud

---
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloud/handler/MyRequestOriginParser.java
@@ -0,0 +1,19 @@
+package com.atguigu.cloud.handler;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+/**
+ * @auther zzyy
+ * @create 2023-11-30 19:33
+ */
+@Component
+public class MyRequestOriginParser implements RequestOriginParser
+{
+    @Override
+    public String parseOrigin(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getParameter("serverName");
+    }
+}
+
---
+++ seata-order-service2001/src/main/java/com/atguigu/cloud/mapper/OrderMapper.java
@@ -0,0 +1,11 @@
+package com.atguigu.cloud.mapper;
+
+/**
+ * @auther zzyy
+ * @create 2023-12-01 17:50
+ */
+import com.atguigu.cloud.entities.Order;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface OrderMapper extends Mapper<Order> {
+}
